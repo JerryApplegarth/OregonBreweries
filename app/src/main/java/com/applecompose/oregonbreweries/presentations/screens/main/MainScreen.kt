844744a8601f4b8d4476cd40edf8c782a4162112
Jerry applegarth <applegarth1630@gmail.com>
2022-08-13 16:47:12 -0700

    basic app working but can't make a list

--- app/src/main/java/com/applecompose/oregonbreweries/presentations/screens/main/MainScreen.kt
+++ app/src/main/java/com/applecompose/oregonbreweries/presentations/screens/main/MainScreen.kt
@@ -1,36 +1,67 @@
 package com.applecompose.oregonbreweries.presentations.screens.main
 
-import android.content.ContentValues.TAG
-import android.util.Log
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.R
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
-import com.applecompose.oregonbreweries.data.DataOrException
-import com.applecompose.oregonbreweries.data.model.BreweriesItem
 import com.applecompose.oregonbreweries.presentations.screens.BreweriesViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 @Composable
 fun MainScreen(navController: NavController, viewModel: BreweriesViewModel = hiltViewModel()) {
-	ShowData(viewModel)
-
-}
-
-@Composable
-fun ShowData(viewModel: BreweriesViewModel) {
 	val brewery = viewModel.data.value.data?.toMutableList()
+
+	
+
 	if (viewModel.data.value.loading == true) {
-		Log.d("LOADING", "ShowData: Loading.....")
-	}else {
+		CircularProgressIndicator()
+	} else {
 		brewery?.forEach { breweryItem ->
-			Log.d("TYPE", "ShowData: ${breweryItem.brewery_type}")
+			val myList = breweryItem.brewery_type
+			val name = breweryItem.name
+			val city = breweryItem.city
+			Scaffold(
+				topBar = {
+					TopAppBar(
+						backgroundColor = MaterialTheme.colors.primary,
+						title = { Text(stringResource(com.applecompose.oregonbreweries.R.string.app_name)) }
+					)
+				}
+			) {
+				LazyColumn(
+					modifier = Modifier.fillMaxWidth(),
+					contentPadding = PaddingValues(16.dp)
+				) {
+					item {
+						Column(
+							modifier = Modifier
+								.fillMaxWidth()
+								.wrapContentHeight()
+								.padding(vertical = 25.dp),
+							horizontalAlignment = Alignment.Start,
+							verticalArrangement = Arrangement.Top
+
+						) {
+							Text(text = myList)
+						}
+
+					}
+				}
+			}
 		}
 	}
 
-
-	Text(text = "Main Screen")
-
 }
+
+
+
+
+
+
