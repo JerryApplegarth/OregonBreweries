35c63cd80ae4b8024881b8e4792ede10b93fd8e0
Jerry applegarth <applegarth1630@gmail.com>
2022-08-15 09:49:05 -0700

    working on expanded card

--- app/src/main/java/com/applecompose/oregonbreweries/presentations/screens/main/MainScreen.kt
+++ app/src/main/java/com/applecompose/oregonbreweries/presentations/screens/main/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.applecompose.oregonbreweries.presentations.screens.main
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,10 +9,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCompositionContext
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -19,6 +24,7 @@ import androidx.navigation.NavController
 import com.applecompose.oregonbreweries.R
 import com.applecompose.oregonbreweries.presentations.screens.BreweriesViewModel
 import com.applecompose.oregonbreweries.ui.theme.newBackgroundColor
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 @Composable
 fun MainScreen(navController: NavController, viewModel: BreweriesViewModel = hiltViewModel()) {
@@ -38,32 +44,29 @@ fun MainScreen(navController: NavController, viewModel: BreweriesViewModel = hil
 		) {
 			item {
 				MyBreweries(viewModel = viewModel)
-
 			}
-
 		}
-
 	}
-
 }
 
 
 @Composable
-fun MyBreweries(viewModel: BreweriesViewModel) {
-
+fun MyBreweries(
+	viewModel: BreweriesViewModel,
+	onClick: () -> Unit = {}
+	) {
 	val brewery = viewModel.data.value.data?.toMutableList()
 	if (viewModel.data.value.loading == true) {
 		CircularProgressIndicator()
 	} else {
 		brewery?.forEach { breweryRow ->
-			Log.d("NAME", "MyBreweries: ${breweryRow.name}")
+
 			Text(
-				text = "Name: ${breweryRow.name}",
+				text =  breweryRow.name,
 				fontSize = 20.sp,
 				fontWeight = FontWeight.Bold
 			)
-
-			Surface() {
+			Surface {
 				Column(
 					modifier = Modifier
 						.fillMaxWidth()
@@ -75,42 +78,38 @@ fun MyBreweries(viewModel: BreweriesViewModel) {
 					Card(
 						modifier = Modifier
 							.padding(4.dp)
-							.clickable { },
-						RoundedCornerShape(16.dp),
-						elevation = 4.dp
-
+							.fillMaxWidth()
+							.clickable {
+									   onClick
+							},
+						shape = MaterialTheme.shapes.large,
+						elevation = 4.dp,
+						backgroundColor = MaterialTheme.colors.newBackgroundColor
 					) {
-						Column() {
+						Column(
+							modifier = Modifier
+								.padding(15.dp)
+								.background(MaterialTheme.colors.newBackgroundColor)
+						) {
 							Text(
-								text = "Name: ${breweryRow.name}",
-								modifier = Modifier
-									.fillMaxWidth()
-									.padding(start = 12.dp)
+								"Type: ${breweryRow.brewery_type}",
+
+								fontSize = 16.sp,
+								fontWeight = FontWeight.Light
 							)
 							Text(
-								text = "Type: ${breweryRow.brewery_type}",
-								modifier = Modifier
-									.fillMaxWidth()
-									.padding(start = 12.dp)
-							)
-							Text(
-								text = "City: ${breweryRow.city}",
-								modifier = Modifier
-									.fillMaxWidth()
-									.padding(start = 12.dp)
+								"Address: ${breweryRow.street}",
+								fontSize = 16.sp,
+								fontWeight = FontWeight.Light
 							)
 						}
-
-
 					}
-
 				}
-
 			}
-
 		}
 	}
 }
+
 
 
 
--- app/src/main/java/com/applecompose/oregonbreweries/presentations/utile/Constants.kt
+++ app/src/main/java/com/applecompose/oregonbreweries/presentations/utile/Constants.kt
@@ -2,5 +2,5 @@ package com.applecompose.oregonbreweries.presentations.utile
 
 object Constants {
 	const val BASE_URL = "https://api.openbrewerydb.org/"
-	const val END_POINT = "breweries/?by_state=oregon"
+	const val END_POINT = "breweries/?by_state=oregon?per_page=50"
 }
