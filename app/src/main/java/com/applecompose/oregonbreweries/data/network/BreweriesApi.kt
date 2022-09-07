22d39c3443cbeb565b150584e445c81d69d82754
Jerry applegarth <applegarth1630@gmail.com>
2022-09-07 05:54:58 -0700

    updated with Constants and END_POINT

--- app/src/main/java/com/applecompose/oregonbreweries/data/network/BreweriesApi.kt
+++ app/src/main/java/com/applecompose/oregonbreweries/data/network/BreweriesApi.kt
@@ -1,12 +1,13 @@
 package com.applecompose.oregonbreweries.data.network
 
 import com.applecompose.oregonbreweries.data.model.Breweries
+import com.applecompose.oregonbreweries.presentations.utile.Constants
 import retrofit2.http.GET
 import javax.inject.Singleton
 
 @Singleton
 interface BreweriesApi {
 
-	@GET("breweries?by_state=oregon&per_page=50")
+	@GET(Constants.END_POINT)
 	suspend fun getAllBreweries(): Breweries
 }
--- app/src/main/java/com/applecompose/oregonbreweries/presentations/utile/Constants.kt
+++ app/src/main/java/com/applecompose/oregonbreweries/presentations/utile/Constants.kt
@@ -2,5 +2,6 @@ package com.applecompose.oregonbreweries.presentations.utile
 
 object Constants {
 	const val BASE_URL = "https://api.openbrewerydb.org/"
-	const val END_POINT = "breweries/?by_state=oregon?per_page=50"
+	const val END_POINT = "breweries?by_state=oregon&per_page=50"
 }
+// breweries?by_state=oregon&per_page=50
